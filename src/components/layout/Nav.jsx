436d16de3805ee54cd41e9b94099fb45a861faa4
ruslana_hural <rusjah1@gmail.com>
2023-11-23 19:07:10 +0100

    created pages, footer and nav file

---
+++ src/components/layout/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className="navbar bg-base-100 h-[10vh]">
+    <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Users</a>
+    </div>
+    <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+            <li><a>Link</a></li>
+            <li><a>Link</a></li>     
+        </ul>
+    </div>
+    </div>
+  )
+}
+
+export default Nav
