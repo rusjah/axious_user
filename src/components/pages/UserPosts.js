ef5852f638d7bcd3fcd517f909388deda76e1335
ruslana_hural <rusjah1@gmail.com>
2023-11-24 17:36:36 +0100

    added userPosts page

--- src/components/pages/UserPosts.js
+++ src/components/pages/UserPosts.js
@@ -3,20 +3,21 @@ import React, { useState } from 'react'
 
 function UserPosts({userID}) {
   const [displayBtn, setDisplayBtn] = useState(true);
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+
 
   const postsGenerate = () => {
-    axios.get(`'https://jsonplaceholder.typicode.com/posts?userId=${userID}`)
+    axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${userID}`)
         .then(res => {
             res.data.map(post => {
-                setPosts(i =>[...i, {
+            setPosts(i =>[...i, {
                     title: post.title,
                     body: post.body,
                 }])
             });
-            console.log(res.data);
         })
         .catch(err => console.log(err))
+        console.log('fun', posts);
     setDisplayBtn(i => false);
   }
 
@@ -27,7 +28,16 @@ function UserPosts({userID}) {
         </div>
         <div style={{display: !displayBtn ? 'inline-block':'none'}}>
             <div className=' flex flex-wrap justify-center items-center gap-[7vh]'>
-              
+              {posts?.map((post, ind) => { 
+                return <div key={ind} className="card w-96 bg-base-100 shadow-xl">
+                  {/* {console.log('bodyyyy',post.title)} */}
+                    <div className="card-body">
+                      <h2 className="card-title">{post.title} llsl</h2>
+                      <p>{post.body}</p>
+                    </div>
+                    {console.log(post, 'from boody')}
+                </div>
+              })}
             </div>                        
         </div>   
     </div>
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -39,7 +39,7 @@ function Users({setUserID}) {
         <div style={{display: !displayBtn ? 'inline-block':'none'}}>
             <div className=' flex flex-wrap justify-center items-center gap-[7vh]'>
                 {users.map((user, ind) => 
-                <div key={ind} className="card w-96 bg-base-100 shadow-xl">
+                (<div key={ind} className="card w-96 bg-base-100 shadow-xl">
                     <div className="card-body">
                         <h2 className="card-title">{user.username}</h2>
                         <h4 className='font-bold'>{user.email}</h4>
@@ -49,7 +49,7 @@ function Users({setUserID}) {
                             <button className="btn btn-primary" onClick={() => {postHandler(user.id)}}>Check posts</button>
                         </div>
                     </div>
-                </div>)}     
+                </div>))}     
             </div>                        
         </div>                                                                                                                         
     </div>
